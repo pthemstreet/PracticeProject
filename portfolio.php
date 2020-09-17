bd83fcbbab3a400387f6566046695f5aeef1879d
Philamon Hemstreet <pthemstreet@crimson.ua.edu>
2020-09-17 16:28:12 -0500

    commit 1

--- portfolio.php
+++ portfolio.php
@@ -4,8 +4,9 @@ require "header.php";
 ?>
 <main>
     <div class="wrapper-main">
-
-
+        <section class="section-default">
+            <p> Placeholder Text </p>
+        </section>
     </div>
 </main>
 <?php
